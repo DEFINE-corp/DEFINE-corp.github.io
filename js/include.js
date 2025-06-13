7e3bc8dd5776571b488fc02dc533cd5a56d86e61
miyeon <meeyun90@gmail.com>
2025-06-13 17:59:09 +0900

    화면 수정

--- js/include.js
+++ js/include.js
@@ -10,62 +10,71 @@ document.addEventListener('DOMContentLoaded', () => {
           window.bindNavEvents();  // 전역에 노출된 함수 호출
           window.bindHeaderScroll();
         }
+
+        // CSS 파일 캐시 무시하고 새로 불러오기
+        const cssLinks = document.querySelectorAll('link[rel="stylesheet"]');
+        cssLinks.forEach(link => {
+          const url = link.getAttribute('href');
+          link.setAttribute('href', `${url}?v=${new Date().getTime()}`);
+        });
+
+        // JS 파일 캐시 무시하고 새로 불러오기
+        const scriptTags = document.querySelectorAll('script');
+        scriptTags.forEach(script => {
+          const url = script.getAttribute('src');
+          if (url) {
+            script.setAttribute('src', `${url}?v=${new Date().getTime()}`);
+          }
+        });
       });
   });
 
   window.loadMainContent = function(pageName, pushState = true) {
-  const main = document.querySelector('main');
-  fetch(`html/content/${pageName}.html`)
-    .then(response => {
-      if (!response.ok) throw new Error('Page not found');
-      return response.text();
-    })
-    .then(data => {
-      main.innerHTML = data;
+    const main = document.querySelector('main');
+    fetch(`html/content/${pageName}.html`)
+      .then(response => {
+        if (!response.ok) throw new Error('Page not found');
+        return response.text();
+      })
+      .then(data => {
+        main.innerHTML = data;
 
-      // const titleMap = {
-      //   home: '홈',
-      //   about: '회사 소개',
-      //   contact: '문의하기'
-      // };
-      // document.title = `${titleMap[pageName] || '페이지'} | 사이트 이름`;
-
-      const body = document.body;
-      if (pageName === 'home') {
-        body.classList.add('main');
-        body.classList.remove('sub');
-        const logoImg = document.querySelector('.logo img');
-        if (logoImg) logoImg.src = '../images/common/logo_white.svg';
-      } else {
-        body.classList.add('sub');
-        body.classList.remove('main');
-        const logoImg = document.querySelector('.logo img');
-        if (logoImg) logoImg.src = '../images/common/logo_blue.svg';
-      }
-
-      if (window.onContentLoaded) {
-        window.onContentLoaded(pageName);
-      }
-
-      // 주소 변경
-      if (pushState) {
-        let newUrl = `/${pageName}`;
+        const body = document.body;
         if (pageName === 'home') {
-          newUrl = '/';
-        } else if (pageName === 'about') {
-          newUrl = '/about-us';
-        } else if (pageName === 'location') {
-          newUrl = '/contact';
-        } else if (pageName === 'contact') {
-          newUrl = '/contact-us';
+          body.classList.add('main');
+          body.classList.remove('sub');
+          const logoImg = document.querySelector('.logo img');
+          if (logoImg) logoImg.src = '../images/common/logo_white.svg';
+        } else {
+          body.classList.add('sub');
+          body.classList.remove('main');
+          const logoImg = document.querySelector('.logo img');
+          if (logoImg) logoImg.src = '../images/common/logo_blue.svg';
         }
-        history.pushState({ pageName }, '', newUrl);
-      }      
-    })
-    .catch(error => {
-      main.innerHTML = `<p>요청한 페이지를 불러올 수 없습니다.</p>`;
-      console.error(error);
-    });
+
+        if (window.onContentLoaded) {
+          window.onContentLoaded(pageName);
+        }
+
+        // 주소 변경
+        if (pushState) {
+          let newUrl = `/${pageName}`;
+          if (pageName === 'home') {
+            newUrl = '/';
+          } else if (pageName === 'about') {
+            newUrl = '/about-us';
+          } else if (pageName === 'location') {
+            newUrl = '/contact';
+          } else if (pageName === 'contact') {
+            newUrl = '/contact-us';
+          }
+          history.pushState({ pageName }, '', newUrl);
+        }
+      })
+      .catch(error => {
+        main.innerHTML = `<p>요청한 페이지를 불러올 수 없습니다.</p>`;
+        console.error(error);
+      });
   };
 
   window.addEventListener('popstate', (event) => {
